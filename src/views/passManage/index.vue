ddc82d9a3d550a755cffc4a8f5b3334d4945c099
zhuwenhao <cy-zhuwh@dfl.com.cn>
2021-10-14 14:34:25 +0800

    密码管理页面

---
+++ src/views/passManage/index.vue
@@ -0,0 +1,234 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-14 10:35:23
+ * @LastEditTime: 2021-10-14 14:22:00
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \gongyu-system\src\views\passManage\index.vue
+-->
+<template>
+  <div class="app-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;"
+    >
+      <el-table-column align="center" label="序号" width="80">
+        <template slot-scope="scope">
+          <span>{{ scope.row.id }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        align="center"
+        class-name="status-col"
+        label="房号"
+        width="110"
+      >
+        <template slot-scope="{ row }">
+          <el-tag :type="row.status | statusFilter">
+            {{ row.fh }}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="120px" align="center" label="姓名">
+        <template slot-scope="scope">
+          <span>{{ scope.row.xm }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="180px" align="center" label="入住时间">
+        <template slot-scope="scope">
+          <span>{{ scope.row.rzsj }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="180px" align="center" label="租约时间">
+        <template slot-scope="scope">
+          <span>{{ scope.row.zysj }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" width="200px" label="房间密码">
+        <template slot-scope="scope">
+          <!-- <svg-icon v-for="n in +scope.row.importance" :key="n" icon-class="star" class="meta-item__icon" /> -->
+          <span>{{ scope.row.csmm }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        align="center"
+        class-name="status-col"
+        label="手机号码"
+        width="200"
+      >
+        <template slot-scope="scope">
+          <!-- <el-tag :type="row.status | statusFilter"> -->
+          <span>{{ scope.row.sjhm }}</span>
+          <!-- </el-tag> -->
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="操作" width="200">
+        <template slot-scope="scope">
+          <el-button
+            type="warning"
+            size="mini"
+            icon="el-icon-refresh"
+            @click="resetPass(scope.row)"
+          >
+            重置密码
+          </el-button>
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-edit-outline"
+            style="marginLeft:0px;marginTop:10px"
+            @click="editPass(scope.row)"
+          >
+            修改密码
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    />
+  </div>
+</template>
+
+<script>
+import { fetchList } from '@/api/article'
+import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+
+export default {
+  name: 'ArticleList',
+  components: { Pagination },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      list: null,
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        page: 5,
+        limit: 10
+      },
+      rows: {}
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    resetPass(row) {
+      console.log(row)
+      this.$confirm('此操作将重置为默认密码, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '重置成功'
+          })
+          row.csmm = '123456'
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消重置'
+          })
+        })
+    },
+    editPass(row) {
+      this.$prompt('请输入需要修改的 6 位纯数字密码', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /^\d{6}$/,
+        inputErrorMessage: '密码格式错误'
+      })
+        .then(({ value }) => {
+          this.$message({
+            type: 'success',
+            message: '密码已更改为 :' + value
+          })
+          row.csmm = value
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消更改'
+          })
+        })
+    },
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+        this.listLoading = false
+      })
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    write(rows) {
+      this.rows = rows
+      this.dialogVisible = true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.edit-input {
+  padding-right: 100px;
+}
+.cancel-btn {
+  position: absolute;
+  right: 15px;
+  top: 10px;
+}
+.info {
+  display: flex;
+  .left {
+    /* display: flex; */
+    width: 80%;
+    div {
+      display: flex;
+    }
+  }
+  .right {
+    width: 20%;
+    height: 100px;
+    img {
+      width: 100%;
+      height: 100px;
+    }
+  }
+}
+</style>
