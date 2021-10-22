8e4b28e8e27216bc7cfd3623bffce1f3dc6b209b
刘传深 <3931021932@qq.com>
2021-10-22 19:10:57 +0800

    123

--- Database/src/db/mongo.js
+++ Database/src/db/mongo.js
@@ -1,89 +1,89 @@
-const {MongoClient,ObjectId} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb')
 
 const config = {
-    url:'mongodb://localhost:27017',
-    dbName:'houselist'
+  url: 'mongodb://localhost:27017',
+  dbName: 'houselist'
 }
 
-async function connect(){
-    try{
-        const client = await MongoClient.connect(config.url,{ useUnifiedTopology: true })
-        const db = client.db(config.dbName);
-        return {client,db}
-    }catch(err){
-        throw new Error(err);
-    }
+async function connect() {
+  try {
+    const client = await MongoClient.connect(config.url, { useUnifiedTopology: true })
+    const db = client.db(config.dbName)
+    return { client, db }
+  } catch (err) {
+    throw new Error(err)
   }
+}
 
 /**
  * 增：添加数据
  * @param {String}          colName     集合名称
  * @param {Object|Array}    data        插入数据
  */
- async function create(colName,data){
-    // 连接数据库
-    const {db,client} = await connect();
+async function create(colName, data) {
+  // 连接数据库
+  const { db, client } = await connect()
 
-    // 获取集合
-    const col = db.collection(colName);
+  // 获取集合
+  const col = db.collection(colName)
 
-    if(!Array.isArray(data)){
-        data = [data];
-    }
-    const result = await col.insertMany(data);
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
+  const result = await col.insertMany(data)
 
-    console.log('create=',result)
+  console.log('create=', result)
 
-    // 关闭数据库连接
-    client.close();
+  // 关闭数据库连接
+  client.close()
 
-    return result;
+  return result
 }
 //   create('user',{})
 
 /**
  * 删：删除数据
  * @param {String} colName  集合名称
- * @param {Object} query    查询条件 
+ * @param {Object} query    查询条件
  */
-async function remove(colName,query){
-    // 连接数据库
-    const {db,client} = await connect();
-    // 获取集合
-    const col = db.collection(colName);
+async function remove(colName, query) {
+  // 连接数据库
+  const { db, client } = await connect()
+  // 获取集合
+  const col = db.collection(colName)
 
-    const result = await col.deleteMany(query)
-    client.close();
-    return result;
+  const result = await col.deleteMany(query)
+  client.close()
+  return result
 }
 
 /**
  * 改：修改数据
  * @param {String} colName  集合名称
- * @param {Object} query    查询条件 
+ * @param {Object} query    查询条件
  * @param {Object} data     更新操作
  */
-async function update(colName,query,data){
-    const {db,client} = await connect()
+async function update(colName, query, data) {
+  const { db, client } = await connect()
 
-    // 获取集合
-    const col = db.collection(colName)
+  // 获取集合
+  const col = db.collection(colName)
 
-    if(typeof query._id === 'string'){
-        // 600a398f7fb70cd40a42e1f9 -> ObjectId("600a398f7fb70cd40a42e1f9")
-        query._id = ObjectId(query._id)
-    }
+  if (typeof query._id === 'string') {
+    // 600a398f7fb70cd40a42e1f9 -> ObjectId("600a398f7fb70cd40a42e1f9")
+    query._id = ObjectId(query._id)
+  }
 
-    const result = await col.updateOne(query,{$set:{'fh':data.fh,'rzsj':data.rzsj,'zysj':data.zysj,'xm':data.xm,'sfzhm':data.sfzhm,'sjhm':data.sjhm,'hx':data.hx,'jfqk':data.jfqk}});
+  const result = await col.updateOne(query, { $set: { 'fh': data.fh, 'rzsj': data.rzsj, 'zysj': data.zysj, 'xm': data.xm, 'sfzhm': data.sfzhm, 'sjhm': data.sjhm, 'hx': data.hx, 'jfqk': data.jfqk }})
 
-    client.close();
-    return result;
+  client.close()
+  return result
 }
 
- /**
+/**
    * 查：查询数据
    * @param {String} colName    集合名称
-   * @param {Object} query      查询条件 
+   * @param {Object} query      查询条件
    * @param {Object} options
         * fields    过滤字段
         * skip      跳过数量（用于数据分页）
@@ -91,57 +91,54 @@ async function update(colName,query,data){
         * sort      排序
    * @return {Array}            返回数据结果
    */
-  async function find(colName,query={},{fields,skip,limit,sort}={}){
-    const {db,client} = await connect()
+async function find(colName, query = {}, { fields, skip, limit, sort } = {}) {
+  const { db, client } = await connect()
 
-    // 获取集合
-    const col = db.collection(colName)
-    if(typeof query._id === 'string'){
-        // 600a398f7fb70cd40a42e1f9 -> ObjectId("600a398f7fb70cd40a42e1f9")
-        query._id = ObjectId(query._id)
-    }
-
-    // 操作数据库
-    let result = col.find(query,{
-        // 过滤字段
-        projection:fields
-    });
-
-    // 获取总数量
-    let count = await result.count();
-
-    // 排序
-    if(sort){ // sort='price' sort='price,1'
-        let [key,val=-1] = sort.trim().split(/\s*,\s*/);//['price'], ['price','1']
-        result = result.sort({
-            [key]:val*1
-        })
-    }
-
-    if(skip){
-        result = result.skip(skip);
-    }
-
-    if(limit){
-        result = result.limit(limit);
-    }
-
-    result = await result.toArray();
-
-    // 关闭连接
-    client.close()
-
-    return {total:count,result};
-    
+  // 获取集合
+  const col = db.collection(colName)
+  if (typeof query._id === 'string') {
+    // 600a398f7fb70cd40a42e1f9 -> ObjectId("600a398f7fb70cd40a42e1f9")
+    query._id = ObjectId(query._id)
   }
 
-  module.exports = {
-      create,
-      remove,
-      update,
-      find
+  // 操作数据库
+  let result = col.find(query, {
+    // 过滤字段
+    projection: fields
+  })
+
+  // 获取总数量
+  const count = await result.count()
+
+  // 排序
+  if (sort) { // sort='price' sort='price,1'
+    const [key, val = -1] = sort.trim().split(/\s*,\s*/)// ['price'], ['price','1']
+    result = result.sort({
+      [key]: val * 1
+    })
   }
 
+  if (skip) {
+    result = result.skip(skip)
+  }
 
+  if (limit) {
+    result = result.limit(limit)
+  }
+
+  result = await result.toArray()
+
+  // 关闭连接
+  client.close()
+
+  return { total: count, result }
+}
+
+module.exports = {
+  create,
+  remove,
+  update,
+  find
+}
 
 //   db.getCollection('password').insert([ { id: '1', fh: '101', rzsj: '2020-1-2', zysj: '2021-12-31', xm: '张三', csmm: '239294', sjhm: '13762547362'},{ id: '2', fh: '102', rzsj: '2021-10-2', zysj: '2021-12-31', xm: '李四', csmm: '831254', sjhm: '13764547362'},{ id: '3', fh: '201', rzsj: '2020-4-10', zysj: '2021-12-31', xm: '王育林', csmm: '439292', sjhm: '1376254362'},{ id: '4', fh: '202', rzsj: '2020-5-5', zysj: '2021-12-31', xm: '江韵琳', csmm: '791224', sjhm: '13362547362'},{ id: '5', fh: '301', rzsj: '2019-3-17', zysj: '2021-12-31', xm: '刘飞', csmm: '632291', sjhm: '13362547362'},{ id: '6', fh: '302', rzsj: '2020-8-5', zysj: '2021-12-31', xm: '王小五', csmm: '291294', sjhm: '13962547362'},{ id: '7', fh: '401', rzsj: '2020-11-25', zysj: '2021-12-31', xm: '卢科研', csmm: '239294', sjhm: '137625237362'},{ id: '8', fh: '402', rzsj: '2017-1-25', zysj: '2021-12-31', xm: '哈力克', csmm: '431204', sjhm: '137625433232'},{ id: '9', fh: '501', rzsj: '2028-9-20', zysj: '2021-12-31', xm: '西坝口', csmm: '021294', sjhm: '13762541262'},{ id: '10', fh: '502', rzsj: '2021-2-27', zysj: '2021-12-31', xm: '阿萨德', csmm: '439292', sjhm: '13126254362'},])
--- Database/src/router/houselist.js
+++ Database/src/router/houselist.js
@@ -1,96 +1,92 @@
-const express = require('express');
-const mongo = require('../db/mongo');
-const router = express.Router();
+const express = require('express')
+const mongo = require('../db/mongo')
+const router = express.Router()
 
-const {formatData,encrypto} = require('../utils')
+const { formatData, encrypto } = require('../utils')
 
-const colNameList = 'list';
-const colNamePsw = 'password';
-const colNameUser = 'user';
+const colNameList = 'list'
+const colNamePsw = 'password'
+const colNameUser = 'user'
 
-    // api/houselist/list
-    // 所有租户分页信息
-    router.get('/list',async (req,res)=>{
-        let {page=1,size=50,sort='regtime',total,name} = req.query;
+// api/houselist/list
+// 所有租户分页信息
+router.get('/list', async(req, res) => {
+  let { page = 1, size = 50, sort = 'regtime', total, name } = req.query
 
-        if(name === '' || name === undefined ){
-            name={}
-        }else{
-            name = {'xm':name}
-        }
-        console.log('name=',name)
+  if (name === '' || name === undefined) {
+    name = {}
+  } else {
+    name = { 'xm': name }
+  }
+  console.log('name=', name)
 
-        let skip = (page-1)*size;
-        let limit = size*1;
-        total = (total=='0'||total=='false') ? false : true;
-        const result = await mongo.find(colNameList,name,{skip,limit,sort,total})
-        console.log('result=',result)
-        res.send(formatData({data:total?result:result.result}))
-    })
+  const skip = (page - 1) * size
+  const limit = size * 1
+  total = !((total == '0' || total == 'false'))
+  const result = await mongo.find(colNameList, name, { skip, limit, sort, total })
+  console.log('result=', result)
+  res.send(formatData({ data: total ? result : result.result }))
+})
 
+// api/houselist/password
+// 所有租户分页信息
+router.get('/password', async(req, res) => {
+  let { page = 1, size = 50, sort = 'regtime', total } = req.query
+  const skip = (page - 1) * size
+  const limit = size * 1
+  total = !((total == '0' || total == 'false'))
+  const result = await mongo.find(colNamePsw, {}, { skip, limit, sort, total })
+  console.log('result=', result)
+  res.send(formatData({ data: total ? result : result.result }))
+})
 
-     // api/houselist/password
-    // 所有租户分页信息
-    router.get('/password',async (req,res)=>{
-        let {page=1,size=50,sort='regtime',total} = req.query;
-        let skip = (page-1)*size;
-        let limit = size*1;
-        total = (total=='0'||total=='false') ? false : true;
-        const result = await mongo.find(colNamePsw,{},{skip,limit,sort,total})
-        console.log('result=',result)
-        res.send(formatData({data:total?result:result.result}))
-    })
+// 新建租客数据写进mongoDB
+router.get('/create', async(req, res) => {
+  const { id, fh, rzsj, zysj, xm, sfzhm, sjhm, hx, jfqk } = req.query
+  try {
+    await mongo.create(colNameList, { id, fh, rzsj, zysj, xm, sfzhm, sjhm, hx, jfqk })
+    res.send(formatData())
+  } catch (err) {
+    res.send(formatData({ code: 400 }))
+  }
+})
 
-    //新建租客数据写进mongoDB
-    router.get('/create',async (req,res)=>{
-        let {id,fh,rzsj,zysj,xm,sfzhm,sjhm,hx,jfqk} = req.query;
-        try{
-            await mongo.create(colNameList,{id,fh,rzsj,zysj,xm,sfzhm,sjhm,hx,jfqk});
-            res.send(formatData())
-        }catch(err){
-            res.send(formatData({code:400}))
-        }
-    })
+// api/houselist/update
+// 把编辑好的数据更新到mongoDB
+router.get('/update', async(req, res) => {
+  const { _id, id, fh, rzsj, zysj, xm, sfzhm, sjhm, hx, jfqk } = req.query
+  // console.log('id=',id)
+  // console.log(_id,id,fh,rzsj,zysj,xm,sfzhm,sjhm,hx,jfqk)
 
-    // api/houselist/update
-    //把编辑好的数据更新到mongoDB
-    router.get('/update',async (req,res)=>{
-        let {_id,id,fh,rzsj,zysj,xm,sfzhm,sjhm,hx,jfqk} = req.query;
-        // console.log('id=',id)
-        // console.log(_id,id,fh,rzsj,zysj,xm,sfzhm,sjhm,hx,jfqk)
+  try {
+    await mongo.update(colNameList, { 'id': id }, { _id, id, fh, rzsj, zysj, xm, sfzhm, sjhm, hx, jfqk })
+    res.send(formatData())
+  } catch (err) {
+    res.send(formatData({ code: 400 }))
+  }
+})
 
-        try{
-            await mongo.update(colNameList,{'id':id},{_id,id,fh,rzsj,zysj,xm,sfzhm,sjhm,hx,jfqk});
-            res.send(formatData())
-        }catch(err){
-            res.send(formatData({code:400}))
-        }
-    })
+// api/houselist/temporary
+// 新建临时看房的密码数据写进mongoDB
+router.get('/temporary', async(req, res) => {
+  const { doorPsw, doorPswTime, houseId, housePsw, housePswTime } = req.query
+  try {
+    await mongo.create(colNamePsw, { doorPsw, doorPswTime, houseId, housePsw, housePswTime })
+    res.send(formatData())
+  } catch (err) {
+    res.send(formatData({ code: 400 }))
+  }
+})
 
+// api/houselist/temporaryList
+// 所有临时密码列表分页信息
+router.get('/temporaryList', async(req, res) => {
+  let { page = 1, size = 50, sort = 'regtime', total } = req.query
+  const skip = (page - 1) * size
+  const limit = size * 1
+  total = !((total == '0' || total == 'false'))
+  const result = await mongo.find(colNamePsw, {}, { skip, limit, sort, total })
+  res.send(formatData({ data: total ? result : result.result }))
+})
 
-    // api/houselist/temporary
-     //新建临时看房的密码数据写进mongoDB
-    router.get('/temporary',async (req,res)=>{
-        let {doorPsw,doorPswTime,houseId,housePsw,housePswTime} = req.query;
-        try{
-            await mongo.create(colNamePsw,{doorPsw,doorPswTime,houseId,housePsw,housePswTime});
-            res.send(formatData())
-        }catch(err){
-            res.send(formatData({code:400}))
-        }
-    })
-
-    // api/houselist/temporaryList
-     // 所有临时密码列表分页信息
-     router.get('/temporaryList',async (req,res)=>{
-        let {page=1,size=50,sort='regtime',total} = req.query;
-        let skip = (page-1)*size;
-        let limit = size*1;
-        total = (total=='0'||total=='false') ? false : true;
-        const result = await mongo.find(colNamePsw,{},{skip,limit,sort,total})
-        res.send(formatData({data:total?result:result.result}))
-    })
-
-
-
-module.exports = router;
+module.exports = router
